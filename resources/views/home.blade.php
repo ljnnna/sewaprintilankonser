83ed8b308685ebcebbe0c6e4e57e9dbf3056e1e8
ljnnna <heymenania@gmail.com>
2025-03-13 02:05:44 +0700

    pweb ulang (masi praktikum 2)

---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <!-- I have not failed. I've just found 10,000 ways that won't work. - Thomas Edison -->
+     Selamat datang di Home {{$nama}}<br>
+     Anda adalah seorang {{$pekerjaan}}<br>
+</div>
